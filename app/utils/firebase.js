d1028ad2b4ca855a3bf3344253b7e9ee6222ac52
Lennin5 <lenninlemus0@gmail.com>
2022-02-16 20:30:15 -0600

    v1.0.6 - Configuración de archivo .env.local y actualización de credenciales firebase para no exponerlas y tener mayor seguridad

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -1,21 +1,23 @@
 import firebase from "firebase/app";
+import {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STOREAGE_BUCKET,  
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID
+} from '@env' // This variables are from .env.local
+// To work, we need to install react-native-dotenv dependence and configure babel.config.js, then only import variables to use. Easy! :)
 
   const firebaseConfig = {
-    apiKey: "AIzaSyCRGshne368uORhEgoF48_V4dOrq8xfdwk",
-    authDomain: "watermeter-fd8ec.firebaseapp.com",
-    databaseURL: "https://watermeter-fd8ec.firebaseio.com",
-    projectId: "watermeter-fd8ec",
-    storageBucket: "watermeter-fd8ec.appspot.com",
-    messagingSenderId: "392890094677",
-    appId: "1:392890094677:web:57c01b865369a59775f0dc",
-
-    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_FIREBASE_STOREAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    // appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STOREAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
   };
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);  
